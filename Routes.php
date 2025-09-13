f9f13ad01d4f79b783be50c54fa9b06ede37714a
AhmadMauludin <ahmadmaldin1799@gmail.com>
2025-09-13 14:24:17 +0700

    Add files via upload

---
+++ Routes.php
@@ -0,0 +1,24 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+// Variabel Filter
+$authFilter = ['filter' => 'auth'];
+
+// Variabel Role
+$admin     = ['filter' => 'role:admin'];
+$user      = ['filter' => 'role:user'];
+$allRole   = ['filter' => 'role:admin, user, kepsek'];
+
+// Login
+$routes->get('/login', 'Auth::login');
+$routes->post('/proses-login', 'Auth::prosesLogin');
+$routes->get('/logout', 'Auth::logout');
+
+// Halaman utama
+$routes->get('/', 'Home::index', $authFilter);
+$routes->get('/dashboard', 'Home::index', $authFilter);
